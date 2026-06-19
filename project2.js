52465cdd7a1529ce3d153d8f2ac5827b65ff80d5
antopaulev <antopaul2022@gmail.com>
2026-06-19 13:53:12 +0530

    calculator using js

---
+++ project2.js
@@ -0,0 +1,38 @@
+const prompt = require("prompt-sync")()
+function getNumber(numString){
+    while(true){
+    num = parseFloat(prompt("Enter number " + numString + " : "))
+    if (isNaN(num))
+        console.log("invalid input")
+    else
+        return num
+}
+}
+const num1 = getNumber("One")
+const num2 = getNumber("Two")
+const operator = prompt("Enter the Sign: ")
+
+let result ;
+let valid = true
+switch (operator){
+    case "+":
+        result = num1 + num2
+        break
+    case "-":
+        result = num1 - num2
+
+        break
+    case "*":
+        result = num1 + num2 
+        break
+    case "/":
+        result = num1 / num2
+        break
+    default:
+        console.log("Invalid")
+        valid = false
+        break
+
+}
+if (valid)
+    console.log( num1, operator, num2, "=", result)
